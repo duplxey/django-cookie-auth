b8022211824644581bd6c9dbee44a213df90e07e
duplxey <nick.tomazic@gmail.com>
2020-11-15 14:12:32 +0100

    Made views.py more readable.

--- api/views.py
+++ api/views.py
@@ -8,8 +8,8 @@ from django.views.decorators.http import require_POST
 
 
 @ensure_csrf_cookie
-def set_csrf_token(request):
-    return JsonResponse({"details": "CSRF cookie set"})
+def ensure_csrf(request):
+    return JsonResponse({"details": "CSRF cookie set."})
 
 
 @require_POST
@@ -17,55 +17,51 @@ def login_view(request):
     data = json.loads(request.body)
     username = data.get('username')
     password = data.get('password')
+
     if username is None or password is None:
-        return JsonResponse({
-            "errors": {
-                "__all__": "Please enter both username and password"
-            }
-        }, status=400)
+        return JsonResponse({"details": "Please provide both username & password."}, status=400)
+
     user = authenticate(username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return JsonResponse({"detail": "Success"})
-    return JsonResponse(
-        {"detail": "Invalid credentials"},
-        status=400,
-    )
+
+    if user is None:
+        return JsonResponse({"detail": "Invalid credentials."}, status=400)
+
+    login(request, user)
+    return JsonResponse({"detail": "Successfully logged in as: " + user.username + "."})
 
 
+@require_POST
 def signup_view(request):
     data = json.loads(request.body)
     username = data.get('username')
     password = data.get('password')
     password1 = data.get('password1')
+
     if username is None or password is None or password1 is None:
-        return JsonResponse({
-            "errors": {
-                "__all__": "Please enter both username and password and password1."
-            }
-        }, status=400)
+        return JsonResponse({"details": "Please provide both username, password & password1."}, status=400)
+
     if User.objects.filter(username=username).exists():
-        return JsonResponse({
-            "errors": {
-                "__all__": "User with this username already exists."
-            }
-        }, status=400)
+        return JsonResponse({"detail": "User with this username already exists."}, status=400)
+
     user = User.objects.create_user(username=username, password=password)
     user.save()
+
     login(request, user)
     return JsonResponse({"details": "You've successfully created an account and logged in."})
 
 
+@require_POST
 def logout_view(request):
     if not request.user.is_authenticated:
-        return JsonResponse({"detail": "You're not logged in."})
+        return JsonResponse({"detail": "You're not logged in."}, status=400)
 
     logout(request)
     return JsonResponse({"detail": "Successfully logged out."})
 
 
 def protected_view(request):
-    if request.user.is_authenticated:
-        return JsonResponse({"detail": "Super secret information. Your username is: " + request.user.username})
-    else:
-        return JsonResponse({"detail": "You are not logged in."})
+    if not request.user.is_authenticated:
+        return JsonResponse({"detail": "You are not logged in."}, status=400)
+
+    return JsonResponse({"detail": "Super secret information. Your username is: " + request.user.username})
+
