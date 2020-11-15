f1de1722dc3914f8b60cd7b1ccf6efbd2f2fc1b3
duplxey <nick.tomazic@gmail.com>
2020-11-15 17:27:45 +0100

    Fixed urls.py.

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('ensure_csrf/', views.ensure_csrf, name='api-ensure-csrf'),
     path('login/', views.login_view, name='api-login'),
-    path('signup/', views.login_view, name='api-signup'),
-    path('logout/', views.login_view, name='api-logout'),
+    path('signup/', views.signup_view, name='api-signup'),
+    path('logout/', views.logout_view, name='api-logout'),
     path('info/', views.info_view, name='api-info'),
 ]
